0185aacc957f923be0932e12c5a3e96ef28c6d6d
koyedev <koyedev@gmail.com>
2024-02-20 22:45:30 +0100

    first commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,28 +3,30 @@ import GoogleProvider from "next-auth/providers/google"
 import { ConnectToDb } from "@utils/database";
 import User from "@models/user";
 
-// Connect to MongoDB before initializing NextAuth
-ConnectToDb().then(() => {
-  const handler = NextAuth({
+const handler = NextAuth({
     providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
     ],
     callbacks: {
       async session({session}) {
         // store the user id from MongoDB to session
-        const sessionUser = await User.findOne({ email: session.user.email });
-        session.user.id = sessionUser._id.toString();
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-        return session;
-      },
-      async signIn({profile}) {
-        try {
+      return session;
+
+    },
+    async signIn({profile}) {
+        try{
+            await ConnectToDb()
+  
+            
           // check if user already exists
           const userExists = await User.findOne({ email: profile.email });
-
+  
           // if not, create a new document and save user in MongoDB
           if (!userExists) {
             await User.create({
@@ -33,16 +35,15 @@ ConnectToDb().then(() => {
               image: profile.picture,
             });
           }
-
-          return true;
-        } catch(err) {
-          // Handle error
-          console.error("Error during sign in:", err);
-          return false;
-        }
-      }
+  
+          return true
+          }catch(err){
+  
+          }
     }
-  });
+    }
 
-  export {handler as GET, handler as POST};
-});
+    
+})
+
+export {handler as GET, handler as POST}
