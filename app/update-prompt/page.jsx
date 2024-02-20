017b9e20910f4060e8425c6743d7ef1457c72562
koyedev <koyedev@gmail.com>
2024-02-20 23:12:34 +0100

    third  commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,39 +1,38 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter , useSearchParams} from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
-
-  const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
   const searchParams = useSearchParams();
-  const promptId = searchParams.get('id')
+  const promptId = searchParams.get("id");
+
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
-     const getPromptDetails = async ( ) => {
-       const response = await fetch(`/api/prompt/${promptId}`)
-       const data = await response.json()
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-       setPost({
-        prompt : data.prompt,
-        tag :data.tag
-       })
-     }
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
 
-     if(promptId) getPromptDetails()
-  }, [promptId])
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-    
-    if(!promptId) return alert("Prompt ID not found")
-    
+
+    if (!promptId) return alert("Missing PromptId!");
+
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
@@ -64,4 +63,4 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
